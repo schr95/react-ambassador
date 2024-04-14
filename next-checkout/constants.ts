f70db7fc160354ae25339c8aab144ef5a455877a
Sara Herrera Ramirez <saraherreraramirez@Rosita.local>
2024-04-14 10:13:44 -0500

    constants

--- next-checkout/constants.ts
+++ next-checkout/constants.ts
@@ -1,4 +1,4 @@
 export default {
     endpoint: 'http://localhost:8000/api/checkout',
-    stripe_key: 'pk_test_51O14MdK9Gfq6HVtwFDc2L4PO16kULV5OVed7yO8e464Aj0GYplJdv1t0ouOgOaUnPcq724XC4iUilkyZhPbWsozT008NnZ8zBT'
+    stripe_key: 'pk_test_51P4mUXI49bkMciF7qVIMzBgDlWZ6VOiu5BbDNEvhSWn69UpwLbyu3vEoiSykfP3UCAMR5VyXZz1c2Tj0NiYsiwUo00rnT3Enyf'
 }
